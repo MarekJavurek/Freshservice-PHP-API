4c8677d520b7f7b9b7b38a428037deb8c4f0ac5a
Marek Javůrek <peanee@gmail.com>
2015-02-22 14:36:09 +0100

    Update LoginCredentials.class.php

--- LoginCredentials.class.php
+++ LoginCredentials.class.php
@@ -4,61 +4,60 @@ namespace Freshservice;
 
 class LoginCredentials {
 
-		// PRIVATE VARIABLES
+	// PRIVATE VARIABLES
+	private $_username = null;
+	private $_password = null;
+	private $_apiKey = null;
 
-		private $_username = null;
-		private $_password = null;
-		private $_apiKey = null;
+	// DECLARE CONSTRUCTORS
 
-		// DECLARE CONSTRUCTORS
+	public static function authenticateWithUserCredentials($username, $password) {
+		$instance = new self();
+		$instance->setUsername($username);
+		$instance->setUsername($password);
+		return $instance;
+	}
 
-    public static function authenticateWithUserCredentials($username, $password) {
-				$instance = new self();
-				$instance->setUsername($username);
-        $instance->setUsername($password);
-				return $instance;
-    }
+	public static function authenticateWithToken($apiKey) {
+		$instance = new self();
+		$instance->setApiKey($apiKey);
+		return $instance;
+	}
 
-    public static function authenticateWithToken($apiKey) {
-        $instance = new self();
-				$instance->setApiKey($apiKey);
-				return $instance;
-    }
+	public function getCurlLoginString() {
+		if ($this->_apiKey == null) {
+			return base64_encode($this->getUsername() . ":" . $this->getPassword());
+		} else {
+			return $this->getApiKey() . ":X";
+		}
+	}
 
-    public function getCurlLoginString() {
-				if ($this->_apiKey == null) {
-						return base64_encode($this->getUsername() . ":" . $this->getPassword());
-				} else {
-            return $this->getApiKey() . ":X";
-				}
-    }
+	// GETTERS
 
-		// GETTERS
+	public function getApiKey() {
+		return $this->_apiKey;
+	}
 
-    public function getApiKey() {
-        return $this->_apiKey;
-    }
+	public function getUsername() {
+		return $this->_username;
+	}
 
-    public function getUsername() {
-        return $this->_username;
-    }
+	public function getPassword() {
+		return $this->_password;
+	}
 
-    public function getPassword() {
-        return $this->_password;
-    }
+	// SETTERS
 
-		// SETTERS
+	public function setApiKey($a) {
+		$this->_apiKey = $a;
+	}
 
-    public function setApiKey($a) {
-        $this->_apiKey = $a;
-    }
+	public function setUsername($a) {
+		$this->_username = $a;
+	}
 
-    public function setUsername($a) {
-        $this->_username = $a;
-    }
-
-    public function setPassword($a) {
-        $this->_password = $a;
-    }
+	public function setPassword($a) {
+		$this->_password = $a;
+	}
     
 }
